6656aaf572c623904b5ae4bad4ab272603c5f729
Nabarun Dutta <nabarundutta@MacBookAir.domain.name>
2025-07-16 01:35:34 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Nabarun',
+    author_email= 'nabarundutta307@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
